42cc2e2cbc555607ab48ed7f4fc5b2f85d486f51
Frank Denis <github@pureftpd.org>
2012-10-11 16:34:02 -0700

    Update the doxydoc

--- plugin-api/search/all_64.js
+++ plugin-api/search/all_64.js
@@ -12,6 +12,7 @@ var searchData=
   ['dcplugin_5fget_5fwire_5fdata_5fmax_5flen',['dcplugin_get_wire_data_max_len',['../plugin_8h.html#a4da25ecdb4a5a83785ba902041490bf1',1,'plugin.h']]],
   ['dcplugin_5finit',['dcplugin_init',['../plugin_8h.html#a717e0b1414d7a2ce020e5f7682e325f5',1,'plugin.h']]],
   ['dcplugin_5flong_5fdescription',['dcplugin_long_description',['../plugin_8h.html#abfeb52c5a34d4943f7458a0303ea9ff5',1,'plugin.h']]],
+  ['dcplugin_5fmain',['DCPLUGIN_MAIN',['../plugin_8h.html#abd4569301e68fc02ae82e34e844c47e6',1,'plugin.h']]],
   ['dcplugin_5fset_5fuser_5fdata',['dcplugin_set_user_data',['../plugin_8h.html#a1e8e3affb0bc7d18f6a751465efa0b14',1,'plugin.h']]],
   ['dcplugin_5fset_5fwire_5fdata',['dcplugin_set_wire_data',['../plugin_8h.html#a847d5de59482585bb14408df5c31134f',1,'plugin.h']]],
   ['dcplugin_5fset_5fwire_5fdata_5flen',['dcplugin_set_wire_data_len',['../plugin_8h.html#a3bd9db3c6c6e4aff815cd001ffacad38',1,'plugin.h']]],
--- plugin-api/search/defines_64.js
+++ plugin-api/search/defines_64.js
@@ -6,6 +6,7 @@ var searchData=
   ['dcplugin_5fget_5fwire_5fdata',['dcplugin_get_wire_data',['../plugin_8h.html#a5d4cb108b4962aaec2a40d9cc53ac127',1,'plugin.h']]],
   ['dcplugin_5fget_5fwire_5fdata_5flen',['dcplugin_get_wire_data_len',['../plugin_8h.html#a4077d54ddee46f3ef23d5a1532733dd4',1,'plugin.h']]],
   ['dcplugin_5fget_5fwire_5fdata_5fmax_5flen',['dcplugin_get_wire_data_max_len',['../plugin_8h.html#a4da25ecdb4a5a83785ba902041490bf1',1,'plugin.h']]],
+  ['dcplugin_5fmain',['DCPLUGIN_MAIN',['../plugin_8h.html#abd4569301e68fc02ae82e34e844c47e6',1,'plugin.h']]],
   ['dcplugin_5fset_5fuser_5fdata',['dcplugin_set_user_data',['../plugin_8h.html#a1e8e3affb0bc7d18f6a751465efa0b14',1,'plugin.h']]],
   ['dcplugin_5fset_5fwire_5fdata',['dcplugin_set_wire_data',['../plugin_8h.html#a847d5de59482585bb14408df5c31134f',1,'plugin.h']]],
   ['dcplugin_5fset_5fwire_5fdata_5flen',['dcplugin_set_wire_data_len',['../plugin_8h.html#a3bd9db3c6c6e4aff815cd001ffacad38',1,'plugin.h']]]
